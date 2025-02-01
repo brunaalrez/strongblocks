4546ed8831ab24e9e18defdb7284742b79575065
brunaalrez <142447083+brunaalrez@users.noreply.github.com>
2025-01-31 21:44:23 -0300

    algumas_mudancas

--- app/produtos/components/blocos.js
+++ app/produtos/components/blocos.js
@@ -1,11 +1,11 @@
 
 function Blocos(arrt) {
     return ( 
-        <div className="text-center border-solid text-black bg-blue-500 p-5 border-yellow-50 shadow-2xl ">
+        <div className="text-center border-solid text-gray-800 bg-yellow-50 ps-5 pe-5 border-gray-300 shadow-2xl ">
             <p>Qual tipo de bloco você precisa?</p>
             <img src="https://placehold.co/200"/>
             <p>{arrt.nomeproduto}</p>
-            <button href="adiquirir_produto" className="bg-white text-color-black">Adiquirir</button>
+            <button href="adiquirir_produto" className="bg-yellow-400 text-color-gray-400">Adiquirir</button>
         </div>
      );
 }
--- app/produtos/components/primeira_produtos.js
+++ app/produtos/components/primeira_produtos.js
@@ -2,14 +2,14 @@ import Blocos from "./blocos";
 
 function PrimeiraPagProduto() {
     return ( 
-        <div className="text-black bg-gray-100">
+        <div className="text-gray-800 bg-gray-300">
             <h1 className="text-center mt-8">Produtos</h1>
             <hr/>
             <p className="text-center">Precisa de mais blocos para sua construção?</p>
             <p className="text-center">Confira agora os diversos blocos que temos!</p>
             
 
-            <div className="flex justify-around">
+            <div className="justify-flex px-30">
                 <Blocos nomeproduto="Bloco A"/>
                 <Blocos nomeproduto="Bloco B"/>
                 <Blocos nomeproduto="Bloco C"/>
