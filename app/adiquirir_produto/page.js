f5f2299ba65389f6726a8bd58384dc882a1ec216
brunaalrez <142447083+brunaalrez@users.noreply.github.com>
2025-02-06 22:13:55 -0300

    minha_parte

--- app/adiquirir_produto/page.js
+++ app/adiquirir_produto/page.js
@@ -1,9 +1,62 @@
-import BlocosAdiquirir from "./components/blocos_pag_adiquirir";
+'use client'
+import { useState } from "react";
+
 
 function PaginaAdiquirirProduto() {
+    const [carrinho, adicionaCarrinho] = useState(0);
+    const [mostra, mostraBotao] = useState(false);
+    const [total, valorTotal] = useState(0)
+    const [precoA, precoProdutoA] = useState(150)
+
+    function handleCarrinho(adicionar = false){
+        let novoCarrinho = carrinho;
+        
+        if(adicionar == true){
+            adicionaCarrinho(carrinho + 1)
+            novoCarrinho++;
+        }else{
+            if(carrinho > 0 && precoA > 0){
+                adicionaCarrinho(carrinho - 1)
+                novoCarrinho--;
+            }
+        }
+        
+        valorTotal(novoCarrinho * precoA)
+        
+
+    }
+    function mostraAgora(){
+        mostraBotao(true)
+    }
     return ( 
         <div>
-            <BlocosAdiquirir/>
+            <p className="p-3 text-lg">Carrinho <strong>{carrinho}</strong> itens</p>
+            <p className="p-3 text-lg">Valor total:<strong> R${total} </strong></p>
+            <div className="text-center border-solid text-gray-800 bg-yellow-50 px-5 border-gray-300 shadow-2xl m-3" >
+                <h1>Bloco Tipo A</h1>
+                
+                <img src="https://placehold.co/400"/>
+                <hr/>
+
+                <h2>Família de 20</h2>
+                <p>Sua largura e formato corresponde a 19cm</p>
+                <p>{precoA}</p>
+                <button onClick={()=>{mostraAgora(); handleCarrinho(true);}}>Adiquirir</button>
+                {
+                    mostra == true &&
+                    <div>
+                        <button className="bg-black-400 text-color-gray-400">Comfirmar Compra</button>
+                        <button onClick={()=>handleCarrinho(false)} className="bg-black-400 text-color-gray-400">Cancelar</button>
+                    </div>
+                }
+                <hr/>
+
+                <h2>Família de 15</h2>
+                <p>Sua largura e formato corresponde a 14cm</p>
+                <button>Adiquirir</button>
+                <button onClick={()=>handleCarrinho(true)} className="bg-black-400 text-color-gray-400">Comfirmar Compra</button>
+                <button onClick={()=>handleCarrinho(false)} className="bg-black-400 text-color-gray-400">Cancelar</button>
+             </div>
         </div>
      );
 }
--- app/produtos/components/blocos.js
+++ app/produtos/components/blocos.js
@@ -1,11 +1,11 @@
 
 function Blocos(arrt) {
     return ( 
-        <div className="text-center border-solid text-gray-800 bg-yellow-50 ps-5 pe-5 border-gray-300 shadow-2xl ">
+        <div className="text-center border-solid text-gray-800 bg-yellow-50 px-5 border-gray-300 shadow-2xl ">
             <p>Qual tipo de bloco você precisa?</p>
             <img src="https://placehold.co/200"/>
             <p>{arrt.nomeproduto}</p>
-            <button href="adiquirir_produto" className="bg-yellow-400 text-color-gray-400">Adiquirir</button>
+            <a href="/adiquirir_produto" className="bg-yellow-400 text-color-gray-400">Adiquirir</a>
         </div>
      );
 }
