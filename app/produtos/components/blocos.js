ff8983655e14dd77f7ca4950e9f2d41357569a71
brunaalrez <142447083+brunaalrez@users.noreply.github.com>
2025-02-20 22:09:42 -0300

    mudanças_simples

--- app/produtos/components/blocos.js
+++ app/produtos/components/blocos.js
@@ -1,25 +1,54 @@
 import { useState } from "react";
 
 function Blocos(arrt) {
-    function adicionaProduto(){
-        if(localStorage.getItem("carrinho") == null){
-            localStorage.setItem("carrinho", JSON.stringify([arrt.produto]))
-        }else{
-            let carrinho = localStorage.getItem("carrinho")
-            carrinho = JSON.parse(carrinho)
-            carrinho.push(arrt.produto)
-            carrinho = JSON.stringify(carrinho)
-            localStorage.setItem("carrinho", carrinho)
+    const [mostrar, alteraMostrar] = useState(false);
+    const [retira, alteraRetira] = useState(true)
+
+    function adicionaProduto() {
+        if (localStorage.getItem("carrinho") == null) {
+            localStorage.setItem("carrinho", JSON.stringify([arrt.produto]));
+        } else {
+            let carrinho = localStorage.getItem("carrinho");
+            carrinho = JSON.parse(carrinho);
+            carrinho.push(arrt.produto);
+            carrinho = JSON.stringify(carrinho);
+            localStorage.setItem("carrinho", carrinho);
         }
     }
-    return ( 
+
+    function Mostrar() {
+        alteraMostrar(!mostrar);
+    }
+
+    function Retira(){
+        alteraRetira(false);
+    }
+
+    return (
         <div className="text-center border-solid text-gray-800 bg-yellow-50 px-5 border-gray-300 shadow-2xl mt-5">
-            
-            <img src={arrt.produto.imagem} className="mt-5"/>
+            <img src={arrt.produto.imagem} className="mt-5" />
             <p>{arrt.produto.nomeproduto}</p>
-            <button onClick={()=> adicionaProduto()} className="bg-yellow-400 text-gray-800 py-1 px-6 rounded-lg shadow-md hover:bg-yellow-500 transition-all no-underline">Adiquirir</button>
+
+            {retira &&(
+                <div>
+            <button onClick={() => {adicionaProduto();Mostrar();Retira()}}
+            className="bg-yellow-400 text-gray-800 py-1 px-6 rounded-lg shadow-md hover:bg-yellow-500 transition-all no-underline">
+                Adquirir
+            </button>
+
+                </div>
+            )
+
+            }
+
+            {mostrar && (
+                <div className="mt-3 text-gray-700">
+                    <h2>Família de 20</h2>
+                    <p>Sua largura e formato corresponde a 19cm</p>
+                </div>
+            )}
         </div>
-     );
+    );
 }
 
 export default Blocos;
--- app/produtos/page.js
+++ app/produtos/page.js
@@ -22,10 +22,7 @@ function Produtos(arrt) {
             <div className="text-gray-800 bg-gray-300">
             <h1 className="text-center mt-8">Produtos</h1>
             <hr/>
-            <p className="text-center">Precisa de mais blocos para sua construção?</p>
-            <p className="text-center">Confira agora os diversos blocos que temos!</p>
             
-
             <div className="justify-flex px-30 text-center">
             <p>Qual tipo de bloco você precisa?</p>
                 <ul>
