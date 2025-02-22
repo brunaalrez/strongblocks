3e92f3095252e44b6e7bf76456038edfed28a659
brunaalrez <142447083+brunaalrez@users.noreply.github.com>
2025-02-21 21:35:01 -0300

    pequenas_mudancas

--- app/produtos/components/blocos.js
+++ app/produtos/components/blocos.js
@@ -1,10 +1,17 @@
 import { useState } from "react";
+import { createContext, useState } from "react";
+import Produtos from "../page";
 
 function Blocos(arrt) {
     const [mostrar, alteraMostrar] = useState(false);
     const [retira, alteraRetira] = useState(true)
 
     const [mostrarBotao, mostrarBotaos] = useState(false);
+
+    const [carrinho, adicionaCarrinho] = useState(0);
+    const [total, valorTotal] = useState(0);
+
+    const InsumoContext = createContext();
 
     function adicionaProduto() {
         if (localStorage.getItem("carrinho") == null) {
@@ -18,6 +25,21 @@ function Blocos(arrt) {
         }
     }
 
+    function handleCarrinho(adicionar = false){
+        let novoCarrinho = carrinho;
+        
+        if(adicionar == true){
+            adicionaCarrinho(carrinho + 1)
+            novoCarrinho++;
+        }else{
+            if(carrinho > 0){
+                adicionaCarrinho(carrinho - 1)
+                novoCarrinho--;
+            }
+        }
+        valorTotal(novoCarrinho)
+    }
+
     function Mostrar() {
         alteraMostrar(!mostrar);
     }
@@ -28,8 +50,12 @@ function Blocos(arrt) {
 
     return (
         <div className="text-center border-solid text-gray-800 bg-yellow-50 px-5 border-gray-300 shadow-2xl mt-5">
+            <div>
             <img src={arrt.produto.imagem} className="mt-5" />
-            <p>{arrt.produto.nomeproduto}</p>
+            <p>{arrt.produto.nome}</p>
+            <div>
+                <Produtos carrinho={carrinho} handleCarrinho={handleCarrinho}/>
+            </div>
 
             {retira &&(
                 <div>
@@ -45,15 +71,19 @@ function Blocos(arrt) {
                     <div>
                         <h2>Família de 20</h2>
                         <p>Sua largura e formato corresponde a 19cm</p>
+                        <button onClick={()=>{ handleCarrinho(true)}} className="py-1 px-6 rounded-lg shadow-md">Adiquirir</button>
                     </div>
                     
                     <div>
                         <h2>Família de 15</h2>
                         <p>Sua largura e formato corresponde a 14cm</p> 
+                        <button onClick={()=>{ handleCarrinho(true)}}  className="py-1 px-6 rounded-lg shadow-md">Adiquirir</button>
                     </div>
                     
                 </div>
             )}
+            </div>
+            
         </div>
     );
 }
--- app/produtos/page.js
+++ app/produtos/page.js
@@ -1,22 +1,24 @@
 'use client'
 import { useState } from "react";
 import Blocos from "./components/blocos";
+import Carrinho from "./components/carrinho";   
 
-function Produtos(arrt) {
+function Produtos({carrinho, handleCarrinho}) {
+
     const [blocos, alteraBloco] = useState([{
         id:0,
         imagem:"https://th.bing.com/th/id/OIP._pnugJyzaPk5tB80UY6WfgHaGF?rs=1&pid=ImgDetMain",
-        nome: "A"
+        nome: "Blocos tipo A"
     },{
         id:1,
         imagem:"https://th.bing.com/th/id/OIP._pnugJyzaPk5tB80UY6WfgHaGF?rs=1&pid=ImgDetMain",
-        nome: "B"
+        nome: "Blocos tipo B"
     },{
         id:2,
         imagem:"https://th.bing.com/th/id/OIP._pnugJyzaPk5tB80UY6WfgHaGF?rs=1&pid=ImgDetMain",
-        nome: "C"
+        nome: "Blocos tipo C"
     }])
-    
+
     return ( 
         <div>
             <div className="text-gray-800 bg-gray-300">
@@ -25,10 +27,12 @@ function Produtos(arrt) {
             
             <div className="justify-flex px-30 text-center">
             <p>Qual tipo de bloco você precisa?</p>
+            <p className="p-3 text-lg">Carrinho <strong>{carrinho}</strong> itens</p>
+            <button className="bg-yellow-400 text-gray-800 py-1 px-6 rounded-lg shadow-md hover:bg-yellow-500 transition-all no-underline">Ver carrinho</button>
                 <ul>
                     {
                         blocos.map((i)=>
-                        <Blocos produto={i}/>
+                        <Blocos key={i.id} produto={i}/>
                     )
                     }
                 </ul>
